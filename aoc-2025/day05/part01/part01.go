324661cffb1dff4083bf53a527e33656d2f5853b
Tomek Rękawek <trekawek@gmail.com>
2025-12-05 10:01:11 +0100

    Day 05 part 01.

--- aoc-2025/day05/part01/part01.go
+++ aoc-2025/day05/part01/part01.go
@@ -1,5 +1,44 @@
 package part01
 
+import (
+	"strconv"
+
+	"github.com/trekawek/advent-of-code/aoc-2025/utils"
+)
+
+func Parse(lines []string) ([]utils.Range, []uint64) {
+	var ranges []utils.Range
+	var ids []uint64
+
+	parsingRanges := true
+	for _, l := range lines {
+		if l == "" {
+			parsingRanges = false
+			continue
+		}
+		if parsingRanges {
+			ranges = append(ranges, *utils.ParseRange(l))
+		} else {
+			i, err := strconv.ParseUint(l, 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			ids = append(ids, i)
+		}
+	}
+	return ranges, ids
+}
+
 func Solve(lines []string) int {
-	return 0
+	c := 0
+	ranges, ids := Parse(lines)
+	for _, i := range ids {
+		for _, r := range ranges {
+			if r.Contains(i) {
+				c++
+				break
+			}
+		}
+	}
+	return c
 }
--- aoc-2025/utils/range.go
+++ aoc-2025/utils/range.go
@@ -1,5 +1,7 @@
 package utils
 
+import "fmt"
+
 type Range struct {
 	From uint64
 	To   uint64
@@ -13,4 +15,20 @@ func (r *Range) Iterate(yield func(uint64) bool) {
 		}
 		i++
 	}
+}
+
+func (r *Range) Contains(i uint64) bool {
+	return i >= r.From && i <= r.To
+}
+
+func ParseRange(s string) *Range {
+	r := Range{}
+	n, err := fmt.Sscanf(s, "%d-%d", &r.From, &r.To)
+	if err != nil {
+		panic(err)
+	}
+	if n == 0 {
+		return nil
+	}
+	return &r
 }
